df99388b70cc7a302277b3f5aab82ded77db8259
Alessandro Fiori <alefiori97@gmail.com>
2023-09-08 11:21:34 +0200

    fix: adjust e2e tests after adding logout button

--- src/e2e/quotes.spec.ts
+++ src/e2e/quotes.spec.ts
@@ -31,75 +31,87 @@ test('has a list of quotes', async ({ page }) => {
 
 test('shows modal when clicking on add quote button', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal')).toBeVisible()
 })
 
 test('add quote modal has an heading', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByRole('heading', { name: 'Add a new quote' })).toBeVisible()
 })
 
 test('add quote modal has a form', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal-form')).toBeVisible()
 })
 
 test('add quote modal has a content textarea', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByLabel('Content')).toBeVisible()
 })
 
 test('add quote modal has a author input', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByLabel('Author')).toBeVisible()
 })
 
 test('add quote modal has an add button', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal-add')).toBeVisible()
 })
 
 test('add quote modal has a cancel button', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal-cancel')).toBeVisible()
 })
 
 test('add quote modal has a close button', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal-close')).toBeVisible()
 })
 
 test('on click on close button, modal is closed', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await page.click('[data-testid="add-modal-close"]')
   await expect(page.getByTestId('add-modal')).not.toBeVisible()
 })
 
 test('on click on cancel button, modal is closed', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await page.click('[data-testid="add-modal-cancel"]')
   await expect(page.getByTestId('add-modal')).not.toBeVisible()
 })
 
 test('add button should be disabled if content is empty', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await expect(page.getByTestId('add-modal-add')).toBeDisabled()
 })
 
 test('add button should be enabled if content is not empty', async ({ page }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await page.getByLabel('Content').fill('__TEST_CONTENT__')
   await expect(page.getByTestId('add-modal-add')).toBeEnabled()
 })
@@ -108,7 +120,8 @@ test('on click on add button, modal is closed and new quote is add. Then quote c
   page,
 }) => {
   await page.goto(localUrl)
-  await page.click('button')
+  const addButton = page.getByRole('button', { name: 'Add Quote' })
+  await addButton.click()
   await page.getByLabel('Content').fill('__TEST_CONTENT__')
   await page.getByLabel('Author').fill('__TEST_AUTHOR__')
   await page.click('[data-testid="add-modal-add"]')
